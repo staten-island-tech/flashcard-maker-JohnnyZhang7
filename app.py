a40faaf74630e5e59e9f130e3e5a4e6062efac1b
JohnnyZhang7 <zjohnnyzhang@gmail.com>
2025-03-25 09:24:38 -0400

    3/25/25 loster

--- app.py
+++ app.py
@@ -16,16 +16,37 @@ class Car:
 my_car = Car("Toyota", "Camry", 2023, "camry_image.jpg")
 print(my_car.display_info())  # Output: 2023 Toyota Camry """
 
+
+import json
+try:
+    with open("teach.json", "r") as file:
+        sentences_data = json.load(file)
+except FileNotFoundError:
+    sentences_data = []
 class teacher:
-    def __init__(self, word, key):
-        self.word = word
+    def __init__(self, key, word):
         self.key = key
-    def display_info(self):
-        return {"word": self.word}
+        self.word = word
+    def sentence(self, words):
+        self.word.append(words)
+        return(self.word)
+    def to_dict(self):
+        return{"key": self.key, "word": self.word}
+"""         part_list_AD = self.word[:1]
+        return part_list_AD
+AD = teacher("AD", ["very good", "not bad", "very bad"])
+print(AD.sentence("")) """ 
 
 teachers = [
-    teacher("")
+    teacher("very good", "to russian"),
+    teacher("not bad", "to russian"),
+    teacher("very bad", "to russian")
 ]
+
+sentences_data = [teacher.to_dict() for teacher in teachers]
+with open("teachers.json", "w") as file:
+    json.dump(sentences_data, file, indent = 4)
+new_flash = teacher("")
 
 """ #LESSON ON CLASS
 class Merchant:
