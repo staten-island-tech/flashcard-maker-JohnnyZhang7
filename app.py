9e38c53f018f8953cc2e7fc1052a3e9c3716682c
JohnnyZhang7 <zjohnnyzhang@gmail.com>
2025-04-04 09:25:43 -0400

    4/4/25 confused

--- app.py
+++ app.py
@@ -25,27 +25,35 @@ import random
 except FileNotFoundError:
     sentences_data = [] """
 class teacher:
-    def __init__(self, subject, language, answer):
-        self.language = language
-        self.subject = subject
+    def __init__(self, question, answer):
+        self.question = question
         self.answer = answer
-    def equation(self, subjects):
-        self.subject.append(subjects)
-        return(self.subject)
+    #def equation(self, subjects):
+        #self.subject.append(subjects)
+        #return(self.subject)
     def to_dict(self):
-        return{"language": self.language, "subject": self.subject, "answer": self.answer}
+        return{"question": self.question, "answer": self.answer}
 
 teachers = []
-random.shuffle(teachers)
+       #teacher("what is 5 * 2", "10")
+while True:
+    question = input("Enter the Question: ")
+    if question == "exit":
+        break
+    answer = input("Enter the Answer: ")
+
+teacher_teacher = teacher(question, answer)
+teachers.append(teacher_teacher)
 
 sentences_data = [teacher.to_dict() for teacher in teachers]
+random.shuffle(teachers) 
 with open("teach.json", "w") as file:
     json.dump(sentences_data, file, indent = 4)
-new_subject = input(teacher("what is 48 + 56", "subject", "104"))
-sentences_data.append(new_subject.to_dict())
+""" new_subject = input(teacher("what is 48 + 56", "104"))
+sentences_data.append(new_subject.to_dict()) """
+with open("teach.json", "w") as file:
+    json.dump(sentences_data, file, indent = 4)
 
-with open("teach.json", "w") as file:
-    json.dump(sentences_data, file, indent = 4)
 
 class student:
     def __init__(self, name):
@@ -64,10 +72,14 @@ class student:
 with open("teach.json", "r") as file:
     cards = open("./teach.json", encoding="utf8")   #stole from movie database
     flashcards = json.load(cards)
-    
-Johnny = student("Johnny")
+
+
+
+
+
+""" Johnny = student("Johnny")
 for card in flashcards:
-    question_list = input(f"question: {card['subject']}")
+    question_list = input(f"question: {card['question']}")
     if question_list == card['answer']:
         print("correct")
         Johnny.co_streak()
@@ -87,15 +99,15 @@ for card in flashcards:
                 Johnny.total_points()
         elif Johnny.current_streak in range(31, 51):
             for i in range(6):
-                Johnny.total_points()
+                Johnny.total_points() """
 
 
-    else:
+"""     else:
         print("incorrect, your streak has been reset")
         Johnny.i_streak()
         print(f"The answer was: {card['answer']}")
     print(f"Your current streak is {Johnny.current_streak}")
-    print(f"Your total points are {Johnny.points}")
+    print(f"Your total points are {Johnny.points}") """
 
 
 """ #LESSON ON CLASS
@@ -111,39 +123,40 @@ Kammy = Merchant("Kammy", ["Penguins", "Whales", "Capybaras"])
 print(Rachel.sell("Human"))
 print(Kammy.sell("Capybaras")) """
 
-"""     teacher("what is 5 * 2", "subject", "10"),
-    teacher("what is 15 - 3", "subject", "12"),
-    teacher("what is 7 + 8", "subject", "15"),
-    teacher("what is 25 * 4", "subject", "100"),
-    teacher("what is 100 / 5", "subject", "20"),
-    teacher("what is 18 + 22", "subject", "40"),
-    teacher("what is 56 / 7", "subject", "8"),
-    teacher("what is 3 * 14", "subject", "42"),
-    teacher("what is 100 - 37", "subject", "63"),
-    teacher("what is 9 * 6", "subject", "54"),
-    teacher("what is 50 + 25", "subject", "75"),
-    teacher("what is 72 / 9", "subject", "8"),
-    teacher("what is 12 * 7", "subject", "84"),
-    teacher("what is 36 / 4", "subject", "9"),
-    teacher("what is 10 + 10", "subject", "20"),
-    teacher("what is 80 - 50", "subject", "30"),
-    teacher("what is 14 * 3", "subject", "42"),
-    teacher("What is the capital city of France?", "subject", "Paris"),
-    teacher("Which continent is known as the 'Dark Continent'?", "subject", "Africa"),
-    teacher("What is the longest river in the world?", "subject", "Amazon River"),
-    teacher("Which country has the largest population?", "subject", "China"),
-    teacher("What is the smallest country in the world by land area?", "subject", "Vatican City"),
-    teacher("Which ocean is the largest by surface area?", "subject", "Pacific Ocean"),
-    teacher("What is the capital city of Japan?", "subject", "Tokyo"),
-    teacher("Which mountain range separates Europe and Asia?", "subject", "Ural Mountains"),
-    teacher("What is the largest desert in the world?", "subject", "Sahara Desert"),
-    teacher("Which country is both an island and a continent?", "subject", "Australia"),
-    teacher("What is the capital city of Canada?", "subject", "Ottawa"),
-    teacher("Which river is the lifeblood of Egypt?", "subject", "Nile River"),
-    teacher("What is the largest island in the Caribbean?", "subject", "Cuba"),
-    teacher("Which country has the most official languages?", "subject", "South Africa"),
-    teacher("Which continent is the Sahara Desert located on?", "subject", "Africa"),
-    teacher("What is the name of the imaginary line that divides the Earth into the Northern and Southern Hemispheres?", "subject", "Equator"),
-    teacher("Which country is known as the Land of the Rising Sun?", "subject", "Japan"),
-    teacher("What is the capital city of Russia?", "subject", "Moscow"),
-    teacher("Which sea is the largest inland body of water?", "subject", "Caspian Sea"), """
+""" teacher("what is 5 * 2", "10"),
+teacher("what is 15 - 3", "12"),
+teacher("what is 7 + 8", "15"),
+teacher("what is 25 * 4", "100"),
+teacher("what is 100 / 5", "20"),
+teacher("what is 18 + 22", "40"),
+teacher("what is 56 / 7", "8"),
+teacher("what is 3 * 14", "42"),
+teacher("what is 100 - 37", "63"),
+teacher("what is 9 * 6", "54"),
+teacher("what is 50 + 25", "75"),
+teacher("what is 72 / 9", "8"),
+teacher("what is 12 * 7", "84"),
+teacher("what is 36 / 4", "9"),
+teacher("what is 10 + 10", "20"),
+teacher("what is 80 - 50", "30"),
+teacher("what is 14 * 3", "42"),
+teacher("What is the capital city of France?", "Paris"),
+teacher("Which continent is known as the 'Dark Continent'?", "Africa"),
+teacher("What is the longest river in the world?", "Amazon River"),
+teacher("Which country has the largest population?", "China"),
+teacher("What is the smallest country in the world by land area?", "Vatican City"),
+teacher("Which ocean is the largest by surface area?", "Pacific Ocean"),
+teacher("What is the capital city of Japan?", "Tokyo"),
+teacher("Which mountain range separates Europe and Asia?", "Ural Mountains"),
+teacher("What is the largest desert in the world?", "Sahara Desert"),
+teacher("Which country is both an island and a continent?", "Australia"),
+teacher("What is the capital city of Canada?", "Ottawa"),
+teacher("Which river is the lifeblood of Egypt?", "Nile River"),
+teacher("What is the largest island in the Caribbean?", "Cuba"),
+teacher("Which country has the most official languages?", "South Africa"),
+teacher("Which continent is the Sahara Desert located on?", "Africa"),
+teacher("What is the name of the imaginary line that divides the Earth into the Northern and Southern Hemispheres?", "Equator"),
+teacher("Which country is known as the Land of the Rising Sun?", "Japan"),
+teacher("What is the capital city of Russia?", "Moscow"),
+teacher("Which sea is the largest inland body of water?", "Caspian Sea"), """
+
